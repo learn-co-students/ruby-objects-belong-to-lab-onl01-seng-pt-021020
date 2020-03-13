23d07b34168205ce812002bf3fd6826a25c989f5
kyle molidor <k_molidor@yahoo.com>
2020-03-13 03:04:29 +0000

    Done.

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -1,22 +1,37 @@
 require "spec_helper"
 
-describe "Post" do
-  let!(:post) { Post.new } 
-  it "has a title" do
-    post.title = "Hello World" 
-    expect(post.title).to eq("Hello World")
+describe "Post" do 
+
+  let!(:post){Post.new("My Blog Post!")}
+  describe "#new" do 
+    it "is initialized with an argument of a title" do 
+      expect{Post.new("Hello World")}.to_not raise_error
+    end
   end
 
-  it "belongs to an author" do 
-    author = Author.new
-    post.author = author
-    expect(post.author).to eq(author)
+  describe "#title" do 
+    it "has a title" do
+      expect(post.title).to eq("My Blog Post!")
+    end
   end
 
-  it "knows the name of the author it belongs to" do 
-    author = Author.new
-    author.name = "Uncle Bob"
-    post.author = author
-    expect(post.author.name).to eq("Uncle Bob")
+  describe "#author" do
+    it "belongs to an author" do 
+      sophie = Author.new("Sophie")
+      post.author = sophie
+      expect(post.author).to eq(sophie)
+    end 
+  end
+
+  describe "#author_name" do
+    it "knows the name of its author" do 
+      sophie = Author.new("Sophie")
+      post.author = sophie
+      expect(post.author_name).to eq("Sophie")
+    end 
+
+    it "returns nil if the post does not have an author" do 
+      expect(post.author_name).to eq nil
+    end
   end
 end
--- spec/song_spec.rb
+++ spec/song_spec.rb
@@ -1,22 +1,38 @@
 require "spec_helper"
 
-describe "Song" do
-  let!(:song) { Song.new } 
-  it "has a title" do
-    song.title = "7/11" 
-    expect(song.title).to eq("7/11")
+describe "Song" do 
+
+  let!(:song) {Song.new("Survivor")}
+  describe "#new" do 
+    it "is initialized with an argument of a name" do 
+      expect{Song.new("Say my Name")}.to_not raise_error
+    end
   end
 
-  it "belongs to an artist" do 
-    artist = Artist.new
-    song.artist = artist
-    expect(song.artist).to eq(artist)
+  describe "#name" do 
+    it "has a name" do
+      expect(song.name).to eq("Survivor")
+    end
   end
 
-  it "knows the name of the artist it belongs to" do 
-    artist = Artist.new
-    artist.name = "Beyonce"
-    song.artist = artist
-    expect(song.artist.name).to eq("Beyonce")
+  describe "#artist" do
+    it "belongs to an artist" do 
+      drake = Artist.new("Drake")
+      song.artist = drake
+      expect(song.artist).to eq(drake)
+    end 
+  end
+
+  describe "#artist_name" do
+    it "knows the name of its artist" do 
+      drake = Artist.new("Drake")
+      song.artist = drake
+      expect(song.artist_name).to eq("Drake")
+    end 
+
+    it "returns nil if the song does not have an artist" do 
+      song = Song.new("Cool for the Summer")
+      expect(song.artist_name).to eq nil
+    end
   end
 end
